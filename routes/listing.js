30f746a4430fa72ac2930cd54baf85ffef33f752
pankaj1-star <pankajrajbhar658@gmail.com>
2025-12-16 13:38:59 +0530

    making changes to upload a file or image instead of image link

--- routes/listing.js
+++ routes/listing.js
@@ -1,14 +1,23 @@
+if(process.env.NODE_ENV !="production") {
+    require("dotenv").config();
+}
+
 const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
-const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 
 const listingController = require("../controllers/listings.js");
+const multer = require('multer');
+const upload = multer("uploads/");
+
 
 router.route("/")
 .get(wrapAsync(listingController.index))
-.post(isLoggedIn, validateListing, wrapAsync(listingController.createListing));
+//.post(isLoggedIn, validateListing, wrapAsync(listingController.createListing));
+.post(upload.single('listing[image]'), (req, res) => {
+    res.send(req.file);
+});
 
 //new route
 router.get("/add", isLoggedIn, listingController.newform);
